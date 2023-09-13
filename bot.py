660f105119ff9678550001aa35b4bbefb6782d89
UTKARSHBOTS <90956407+utkarsh212646@users.noreply.github.com>
2023-09-13 09:19:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,53 +1,139 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+#    This file is part of the ChannelAutoForwarder distribution (https://github.com/utkarsh212646/Telegraph_uploader).
+#    Copyright (c) 2021 Rithunand
+#    
+#    This program is free software: you can redistribute it and/or modify  
+#    it under the terms of the GNU General Public License as published by  
+#    the Free Software Foundation, version 3.
+# 
+#    This program is distributed in the hope that it will be useful, but 
+#    WITHOUT ANY WARRANTY; without even the implied warranty of 
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+#    General Public License for more details.
+# 
+#    License can be found in < https://github.com/utkarsh212646/Telegraph_Uploader/blob/main/License> 
 
-Tgraph = Client("Your_Telegram_API_ID", "Your_Telegram_API_HASH", bot_token="Your_Bot_Token")
+import os
+from telegraph import upload_file
+import pyrogram
+from pyrogram import filters, Client
+from sample_config import Config
+from pyrogram.types import (
+    InlineQueryResultArticle, InputTextMessageContent,
+    InlineKeyboardMarkup, InlineKeyboardButton,
+    CallbackQuery, InlineQuery)
+
+Tgraph = Client(
+   "Telegra.ph Uploader",
+   api_id=Config.APP_ID,
+   api_hash=Config.API_HASH,
+   bot_token=Config.TG_BOT_TOKEN,
+)
+
+@Tgraph.on_message(filters.photo)
+async def uploadphoto(client, message):
+  msg = await message.reply_text("`Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅ`")
+  userid = str(message.chat.id)
+  img_path = (f"./DOWNLOADS/{userid}.jpg")
+  img_path = await client.download_media(message=message, file_name=img_path)
+  await msg.edit_text("`Tʀʏɪɴɢ Tᴏ Uᴘʟᴏᴀᴅ.....`")
+  try:
+    tlink = upload_file(img_path)
+  except:
+    await msg.edit_text("`Something went wrong`") 
+  else:
+    await msg.edit_text(f"https://telegra.ph{tlink[0]}")     
+    os.remove(img_path) 
+
+@Tgraph.on_message(filters.animation)
+async def uploadgif(client, message):
+  if(message.animation.file_size < 5242880):
+    msg = await message.reply_text("`Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅ`")
+    userid = str(message.chat.id)
+    gif_path = (f"./DOWNLOADS/{userid}.mp4")
+    gif_path = await client.download_media(message=message, file_name=gif_path)
+    await msg.edit_text("`Tʀʏɪɴɢ Tᴏ Uᴘʟᴏᴀᴅ.....`")
+    try:
+      tlink = upload_file(gif_path)
+      await msg.edit_text(f"https://telegra.ph{tlink[0]}")   
+      os.remove(gif_path)   
+    except:
+      await msg.edit_text("Something really Happend Wrong...") 
+  else:
+    await message.reply_text("Size Should Be Less Than 5 mb")
+
+@Tgraph.on_message(filters.video)
+async def uploadvid(client, message):
+  if(message.video.file_size < 5242880):
+    msg = await message.reply_text("`Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅ`")
+    userid = str(message.chat.id)
+    vid_path = (f"./DOWNLOADS/{userid}.mp4")
+    vid_path = await client.download_media(message=message, file_name=vid_path)
+    await msg.edit_text("`Tʀʏɪɴɢ Tᴏ Uᴘʟᴏᴀᴅ.....`")
+    try:
+      tlink = upload_file(vid_path)
+      await msg.edit_text(f"https://telegra.ph{tlink[0]}")     
+      os.remove(vid_path)   
+    except:
+      await msg.edit_text("Something really Happend Wrong...") 
+  else:
+    await message.reply_text("Size Should Be Less Than 5 mb")
+
+@Tgraph.on_message(filters.command(["start"]))
+async def home(client, message):
+  buttons = [[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('Close', callback_data='close')
+    ],
+    [
+        InlineKeyboardButton('Our Channel', url='http://telegram.me/Thealphabotz'),
+        InlineKeyboardButton('Support', url='https://t.me/+n5UitRf-oDpmNzM1')
+    ]]
+  reply_markup = InlineKeyboardMarkup(buttons)
+  await Tgraph.send_message(
+        chat_id=message.chat.id,
+        text="""<b>Hey Bro ,
+        
+I m a telegraph Uploader That Can Upload Photo, Video And Gif
+        
+Simply send me photo, video or gif to upload to Telegraph Url
+        
+By @TheAlphaBotz</b>""",
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=message.message_id
+    )
 
 @Tgraph.on_message(filters.command(["help"]))
 async def help(client, message):
-    buttons = [[
+  buttons = [[
         InlineKeyboardButton('Home', callback_data='home'),
         InlineKeyboardButton('Close', callback_data='close')
     ],
     [
         InlineKeyboardButton('Our Channel', url='http://telegram.me/Thealphabotz')
     ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await Tgraph.send_message(
+  reply_markup = InlineKeyboardMarkup(buttons)
+  await Tgraph.send_message(
         chat_id=message.chat.id,
         text="""There Is Nothing To Know More,
         
 Just Send Me A Video/gif/photo Upto 5mb.
 
-I'll upload it to telegra.ph and give you the direct link""",
+i'll upload ut to telegra.ph and give you the direct link""",
         reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=message.message_id
-    )
-
+    )                           
 @Tgraph.on_callback_query()
 async def button(Tgraph, update):
-    cb_data = update.data
-    if "help" in cb_data:
+      cb_data = update.data
+      if "help" in cb_data:
         await update.message.delete()
         await help(Tgraph, update.message)
-    elif "close" in cb_data:
-        await update.message.delete()
-    elif "home" in cb_data:
+      elif "close" in cb_data:
+        await update.message.delete() 
+      elif "home" in cb_data:
         await update.message.delete()
         await home(Tgraph, update.message)
-
-@Tgraph.on_message(filters.private & ~filters.channel)
-async def start_message(client, message):
-    # Send the start message with the image and force subscription
-    await Tgraph.send_photo(
-        chat_id=message.chat.id,
-        photo="https://telegra.ph/file/e7bece928a05168a46322.jpg",
-        caption="Welcome to the bot!\n\nPlease join our channel @thealphabotz to use the bot.",
-        reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton('Join Channel', url='https://t.me/thealphabotz')]
-        ]),
-        reply_to_message_id=message.message_id
-    )
 
 Tgraph.run()
